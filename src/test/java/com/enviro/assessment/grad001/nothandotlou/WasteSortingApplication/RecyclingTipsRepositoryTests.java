e427824f6df70f5daf2784e7a8e69c85a5dd65a5
nothando-tlou <nothandotlou21@gmail.com>
2024-04-25 12:04:41 +0200

    RecyclingRepositoryTests

--- src/test/java/com/enviro/assessment/grad001/nothandotlou/WasteSortingApplication/RecyclingTipsRepositoryTests.java
+++ src/test/java/com/enviro/assessment/grad001/nothandotlou/WasteSortingApplication/RecyclingTipsRepositoryTests.java
@@ -2,11 +2,9 @@ package com.enviro.assessment.grad001.nothandotlou.WasteSortingApplication;
 
 
 import com.enviro.assessment.grad001.nothandotlou.WasteSortingApplication.api.model.RecyclingTips;
-import com.enviro.assessment.grad001.nothandotlou.WasteSortingApplication.api.model.WasteCategory;
 import com.enviro.assessment.grad001.nothandotlou.WasteSortingApplication.api.repository.RecyclingTipsRepository;
-import com.enviro.assessment.grad001.nothandotlou.WasteSortingApplication.api.repository.WasteCategoryRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -17,10 +15,23 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class RecyclingTipsRepositoryTests {
-    private RecyclingTipsRepository recyclingRepository;
 
     @Autowired
-    public RecyclingTipsRepositoryTests(RecyclingTipsRepository recyclingRepository){
-        this.recyclingRepository = recyclingRepository;
+    private RecyclingTipsRepository repository;
+
+    @Test
+    public void RecyclingTipsRepository_testSave_ReturnNotNull(){
+
+        RecyclingTips recyclingTips = RecyclingTips.builder()
+                .tip("Recycling Tip")
+                .build();
+
+        RecyclingTips savedRecyclingTips = repository.save(recyclingTips);
+
+        Assertions.assertThat(savedRecyclingTips).isNotNull();
+        Assertions.assertThat(savedRecyclingTips.getId()).isNotNull();
+
+
     }
+
 }
