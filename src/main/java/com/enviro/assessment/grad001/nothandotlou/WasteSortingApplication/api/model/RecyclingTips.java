846381c257a22bb0f4e36aff9b4f321f6f4774d1
nothando-tlou <nothandotlou21@gmail.com>
2024-04-25 08:54:12 +0200

    RecyclingRepositoryTests

--- src/main/java/com/enviro/assessment/grad001/nothandotlou/WasteSortingApplication/api/model/RecyclingTips.java
+++ src/main/java/com/enviro/assessment/grad001/nothandotlou/WasteSortingApplication/api/model/RecyclingTips.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Builder;
 
 
+@Builder
 @Entity
 public class RecyclingTips {
 
